a2e397c9b777375bf994d673f03170294d09576b
Sidd <sjasti@securityps.com>
2020-06-04 18:07:43 -0500

    nlog file added for logging

--- HR_Management_System/HR_Management_System/Program.cs
+++ HR_Management_System/HR_Management_System/Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace HR_Management_System
 {
@@ -13,14 +15,19 @@ namespace HR_Management_System
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                }); 
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+       WebHost.CreateDefaultBuilder(args)
+       .ConfigureLogging((hostingContext, logging) =>
+       {
+           logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+           logging.AddConsole();
+           logging.AddDebug();
+           logging.AddEventSourceLogger();
+           logging.AddNLog();
+       })
+       .UseStartup<Startup>();
     }
 }
